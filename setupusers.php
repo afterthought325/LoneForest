a8b430c425a51a5590bfc15b2687e15d4300914a
Noah Siano <nsiano@bellsouth.net>
2017-04-03 20:05:31 -0500

    Added ability to register a user. Needs better integration, but the basics are working

--- setupusers.php
+++ setupusers.php
@@ -17,7 +17,9 @@ $query = "CREATE TABLE users (
    forename VARCHAR(32) NOT NULL,
    surname VARCHAR(32) NOT NULL,
    username VARCHAR(32) NOT NULL UNIQUE,
-   password VARCHAR(32) NOT NULL
+   password VARCHAR(32) NOT NULL,
+   security1 VARCHAR(32) NOT NULL,
+   security2 VARCHAR(32) NOT NULL
 )";
 
 $result = $connection->query($query);
@@ -28,24 +30,28 @@ $forename = 'Bill';
 $surname = 'Smith';
 $username = 'bsmith';
 $password = 'mysecret';
+$security1 = 'Mom';
+$security2 = 'Street';
 
 $token = hash($algo, "$salt1$password$salt2");
 
-add_user($connection, $forename, $surname, $username, $token);
+add_user($connection, $forename, $surname, $username, $token, $security1, $security2);
 
 $forename = 'Pauline';
 $surname = 'Jones';
 $username = 'pjones';
 $password = 'acrobat';
+$security1 = 'Mom';
+$security2 = 'Street';
 
 $token = hash($algo, "$salt1$password$salt2");
 
-add_user($connection, $forename, $surname, $username, $token);
+add_user($connection, $forename, $surname, $username, $token, $security1, $security2);
 
 
-function add_user($connection, $fn, $sn, $un, $pw)
+function add_user($connection, $fn, $sn, $un, $pw, $sq1, $sq2)
 {
-   $query = "INSERT INTO users VALUES('$fn', '$sn', '$un', '$pw')";
+   $query = "INSERT INTO users VALUES('$fn', '$sn', '$un', '$pw', '$sq1', '$sq2')";
    $result = $connection->query($query);
    if (!$result) die($connection->error);
 }
