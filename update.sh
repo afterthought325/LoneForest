24246e3e92c4de309a3cf076df4f6a391acea068
Chaise Farrar <chaise.farrar@gmail.com>
2017-04-04 17:51:20 -0500

    Updated update.sh ;) to be more verbose

--- update.sh
+++ update.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-
-cd ~./public_html/LoneForest/
 git pull
 echo "Setting the permissions..."
-chmod -R u+rwX,go+rX,go-w .
+chmod -Rc --preserve-root u+rwX,go+rX,go-w *
 echo "The files have been updated from the github repo!"
-
