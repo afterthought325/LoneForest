92a265d7ada2702b19a81e9280459897b8874f82
Noah Siano <nsiano@bellsouth.net>
2017-04-10 21:37:57 -0500

    Maybe removed login.php from tracking??????

--- login.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $hn = 'localhost';
-  $db = 'dcspa'; // your NetID
-  $un = 'dcspa';  // your NetID
-  $pw = 'loneforest'; // your MySQL password on pluto
-  $salt1 = "tf&u*l";
-  $salt2 = "#fe)@";
-  $algo = 'md5'
-?>
