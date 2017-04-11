f8f3680d3d30996df1b50ac1269ded39affad810
Noah Siano <nsiano@bellsouth.net>
2017-04-10 21:41:07 -0500

    possibly deleting login.php :)

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+  $hn = 'localhost';
+  $db = 'dcspa'; // your NetID
+  $un = 'dcspa';  // your NetID
+  $pw = 'loneforest'; // your MySQL password on pluto
+  $salt1 = "tf&u*l";
+  $salt2 = "#fe)@";
+  $algo = 'md5'
+?>
