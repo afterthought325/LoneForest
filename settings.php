a270a64df3257e81f0a80ba6bea3780cea38e940
Unknown <logan.smith.5@gmail.com>
2017-04-10 22:12:11 -0500

    Needs to save color in cookie

--- settings.php
+++ settings.php
@@ -1,23 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
+<html>
+  
+        <meta charset="utf-8">
         <title>Settings Page</title>
-    </head>
-    <body>
+	<style>
+	
+	</style>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" id = "ourTheme" href="https://www.w3schools.com/lib/w3-theme-grey.css">
+ 	
+
+	<body class = "w3-theme-d4 w3-card-3">
+		<div zclass = "w3-main" >
+			<div class="w3-container w3-theme-d4 w3-card-3">
+				<h1>Settings Page</h1>
+			</div>
+	
         <?php
-       session_start();
+               session_start();
         
         //if (isset($_SESSION["username"]))
-    	//{   
-        	echo"<h1>Settings Page</h1>";
-   		echo"<p>Change color theme: [Color Thing]</p>";
-		echo"<button type='button'>Restart Progress</button>       ";
-		echo"<button type='button'>Delete User Account</button>     ";
-		echo"<button type='button'>Change User Password</button>";
-		echo"<br><br><button onclick='goBack()'>Go Back</button>";
-
-
+    	//{   =
+		echo"<div style = 'padding:20px'class = 'w3-cell-row w3-theme-d3'>";
+   		echo"<br><p>Change color theme: 
+		<select class = 'w3-theme-d3' id='select' onchange = 'updateTheme(this.value)'>
+			<option value='grey' selected='selected' >grey</option>
+  			<option value='red'>red</option>
+ 			<option value='teal' >teal</option>
+  			<option value='blue-grey'>blue-grey</option>
+		</select></p>";
+		echo"<button class = 'w3-theme-d3' type='button'>Restart Progress</button>       ";
+		echo"<button class = 'w3-theme-d3' type='button'>Delete User Account</button>     ";
+		echo"<button class = 'w3-theme-d3' type='button'>Change User Password</button>";
+		echo"<br><br><button class = 'w3-theme-d3' onclick='goBack()'>Go Back</button><br><br>";
+		echo"</div>";
             
                 
         
@@ -27,5 +43,18 @@
         //}
         //} 
         ?>
+	<script>
+		function goBack() {
+    			window.history.back();
+		}
+		function updateTheme(theme) {
+			document.getElementById("ourTheme").href ="https://www.w3schools.com/lib/w3-theme-"+theme+".css";
+			//document.write(document.getElementById("ourTheme").href);
+			
+		}
+		
+	</script>
+		</div>
+	</div>
     </body>
 </html>
