def76500f4e16ebf2241d00a11bd61516a9d8f1a
Unknown <logan.smith.5@gmail.com>
2017-04-12 10:29:39 -0500

    Updated Settings and added new js file
    Now with cookies to allow easy theme change. Must src = .../changeTheme.js to use changeTheme() function.

--- settings/index.php
+++ settings/index.php
@@ -1,6 +1,8 @@
-<!DOCTYPE html>
+
 <html>
-  
+  <head>
+  		<script src = "/LoneForest/js/changeTheme.js" type="text/javascript"></script>
+		  
         <meta charset="utf-8">
         <title>Settings Page</title>
 	<style>
@@ -8,9 +10,10 @@
 	</style>
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="stylesheet" id = "ourTheme" href="https://www.w3schools.com/lib/w3-theme-grey.css">
- 	
-
+</head>
+	<script>changeTheme();</script>
 	<body class = "w3-theme-d4 w3-card-3">
+
 		<div zclass = "w3-main" >
 			<div class="w3-container w3-theme-d4 w3-card-3">
 				<h1>Settings Page</h1>
@@ -23,11 +26,19 @@
     	//{   =
 		echo"<div style = 'padding:20px'class = 'w3-cell-row w3-theme-d3'>";
    		echo"<br><p>Change color theme: 
-		<select class = 'w3-theme-d3' id='select' onchange = 'updateTheme(this.value)'>
-			<option value='grey' selected='selected' >grey</option>
-  			<option value='red'>red</option>
- 			<option value='teal' >teal</option>
-  			<option value='blue-grey'>blue-grey</option>
+		<select class = 'w3-theme-d3' id='mySelect' onchange = 'updateTheme(this.value)'>
+			<option value='grey'>Grey</option>
+			<option value='blue-grey'>Blue-Grey</option>
+  			<option value='red'>Red</option>
+			<option value='pink'>Pink</option>
+  			<option value='deep-purple'>Purple</option>
+ 			<option value='teal'>Teal</option>
+			<option value='cyan'>Cyan</option>
+  			
+			
+  			
+
+
 		</select></p>";
 		echo"<button class = 'w3-theme-d3' type='button'>Restart Progress</button>       ";
 		echo"<button class = 'w3-theme-d3' type='button'>Delete User Account</button>     ";
@@ -49,9 +60,21 @@
 		}
 		function updateTheme(theme) {
 			document.getElementById("ourTheme").href ="https://www.w3schools.com/lib/w3-theme-"+theme+".css";
-			//document.write(document.getElementById("ourTheme").href);
+			document.cookie = "theme="+theme;
+		}
+		function updateSelect(){
+			var val = getCookie("theme");
+			var sel = document.getElementById("mySelect");
+			var opts = sel.options;
+			for (var opt, j = 0; opt = opts[j]; j++) {
+				if (opt.value == val) {
+					sel.selectedIndex = j;
+					break;
+			}
+		}
 			
 		}
+		updateSelect();
 		
 	</script>
 		</div>
