4cedf8479f9f35a52bed61868d17ca2c64e1288e
Noah Siano <nsiano@bellsouth.net>
2017-04-17 21:58:41 -0500

    Fixed logout redirect link... I think :)

--- logout/index.php
+++ logout/index.php
@@ -3,7 +3,7 @@ session_start();
 $_SESSION['logged_in'] = False;
 $_SESSION['username'] = 'no username';
 $_SESSION['isAdmin'] = False;
-header("Location: /LoneForest/");
+header("Location: ../");
 exit();
 
 ?>
