8bc042602875216247859daab611c37fac188eec
Noah Siano <nsiano@bellsouth.net>
2017-04-17 22:07:48 -0500

    Fixed formatting

--- settings/index.php
+++ settings/index.php
@@ -1,59 +1,48 @@
-
+<?php session_start(); ?>
+<!DOCTYPE html>
 <html>
   <head>
-  		<script src = "/LoneForest/js/changeTheme.js" type="text/javascript"></script>
-		  
+  		<script src="/LoneForest/js/changeTheme.js" type="text/javascript"></script>
+
         <meta charset="utf-8">
         <title>Settings Page</title>
 	<style>
-	
-	</style>
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" id = "ourTheme" href="https://www.w3schools.com/lib/w3-theme-grey.css">
-</head>
-	<?php session_start(); ?>
-	<script>changeTheme();</script>
-	<body class = "w3-theme-d4 w3-card-3">
 
-		<div zclass = "w3-main" >
+	</style>
+	<link rel="stylesheet" href = "https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" id="ourTheme" href="https://www.w3schools.com/lib/w3-theme-grey.css">
+</head>
+	<script>changeTheme();</script>
+	<body class="w3-theme-d4 w3-card-3">
+
+		<div class="w3-main" >
 			<div class="w3-container w3-theme-d4 w3-card-3">
 				<h1>Settings Page</h1>
 			</div>
-	
+
         <?php
-               
-        
+
+
         //if (isset($_SESSION["username"]))
     	//{   =
-		echo"<div style = 'padding:20px'class = 'w3-cell-row w3-theme-d3'>";
-   		echo"<br><p>Change color theme: 
-		<select class = 'w3-theme-d3' id='mySelect' onchange = 'updateTheme(this.value)'>
-            <option class='w3-btn' value='grey'>Grey</option>
-			<option class='w3-btn' value='blue-grey'>Blue-Grey</option>
-  			<option class='w3-btn' value='red'>Red</option>
-			<option class='w3-btn' value='pink'>Pink</option>
-  			<option class='w3-btn' value='deep-purple'>Purple</option>
- 			<option class='w3-btn' value='teal'>Teal</option>
-			<option class='w3-btn' value='cyan'>Cyan</option>
-  			
-			
-  			
+		echo "<div style='padding:20px' class='w3-cell-row w3-theme-d3'>";
+ 		echo "<br><p>Change color theme:
+          <select class='w3-theme-d3' id='mySelect' onchange='updateTheme(this.value)'>
+          <option class='w3-btn' value='grey'>Grey</option>
+      		<option class='w3-btn' value='blue-grey'>Blue-Grey</option>
+          <option class='w3-btn' value='red'>Red</option>
+      		<option class='w3-btn' value='pink'>Pink</option>
+          <option class='w3-btn' value='deep-purple'>Purple</option>
+          <option class='w3-btn' value='teal'>Teal</option>
+      		<option class='w3-btn' value='cyan'>Cyan</option>
+          </select></p>";
+		echo "<button class='w3-theme-d3' type='button'>Restart Progress</button>       ";
+		echo "<button class='w3-theme-d3' type='button'>Delete User Account</button>     ";
+		echo "<button class='w3-theme-d3' type='button'>Change User Password</button>";
+		echo "<br><br><button class='w3-theme-d3' onclick='goBack()'>Go Back</button><br><br>";
+		echo "</div>";
 
 
-		</select></p>";
-		echo"<button class = 'w3-theme-d3' type='button'>Restart Progress</button>       ";
-		echo"<button class = 'w3-theme-d3' type='button'>Delete User Account</button>     ";
-		echo"<button class = 'w3-theme-d3' type='button'>Change User Password</button>";
-		echo"<br><br><button class = 'w3-theme-d3' onclick='goBack()'>Go Back</button><br><br>";
-		echo"</div>";
-            
-                
-        
-        //else{
-        //    echo '<p style="color: red"> Hey! You\'re not logged in! </p>';
-        //  echo "<a href = 'login_page.php' > Click here to log in </a>";
-        //}
-        //} 
         ?>
 	<script>
 		function goBack() {
@@ -77,7 +66,7 @@
 					sel.selectedIndex = j;
 					break;
 				}
-			}	
+			}
 		}
 		updateSelect();
 	</script>
