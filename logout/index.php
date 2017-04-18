fc554bae8c46f9d881d2b20503db0040b50d6881
Noah Siano <nsiano@bellsouth.net>
2017-04-18 15:16:42 -0500

    Changed admin session variable to string

--- logout/index.php
+++ logout/index.php
@@ -2,7 +2,7 @@
 session_start();
 $_SESSION['logged_in'] = False;
 $_SESSION['username'] = 'no username';
-$_SESSION['isAdmin'] = False;
+$_SESSION['isAdmin'] = "False";
 header("Location: ../");
 exit();
 
