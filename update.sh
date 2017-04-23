5ad70e40a4d28556b0033f03306544bf0f2f9833
Noah Siano <nsiano@bellsouth.net>
2017-04-23 00:47:41 -0500

    Pushing chaise's change to update.sh

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 
 git pull
 echo "Setting the permissions..."
-chmod -Rc --preserve-root u+rwX,go+rX,go-w *
+chmod -Rc --preserve-root --verbose u+rwX,go+rX,go-w *
 echo "The files have been updated from the github repo!"
